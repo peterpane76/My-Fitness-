3631615b448f783e3b360b1166537c8963929f8d
peterpane76 <finnschachtschneider@icloud.com>
2026-08-27 16:26:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='my-workout-v5';
+const ASSETS=["./", "./index.html", "./sw.js", "./schulterdruecken.jpg", "./seitheben.jpg", "./reverse-fly.jpg", "./face-pulls.jpg", "./klimmzug-breit.jpg", "./klimmzug-normal.jpg", "./klimmzug-eng.jpg", "./chin-ups.jpg", "./dips.jpg", "./muscle-overview.png"];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
